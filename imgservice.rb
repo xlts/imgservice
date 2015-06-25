97f59773da1dbde5485e7570d27c81a8e04d05b2
Mariusz Hausenplas <xelitas@gmail.com>
2015-06-25 11:04:16 +0200

    added request param

--- imgservice.rb
+++ imgservice.rb
@@ -3,3 +3,13 @@ require 'sinatra'
 get '/' do 
 	"<img src='http://lorempixel.com/400/200'> </img> "
 end
+
+get '/:abv' do
+	abv = params[:abv]
+	
+	if abv < 5 
+		"<img src='http://lorempixel.com/400/200/sports/Abv-Smaller-Than-5/'> </img> "
+	else
+		"<img src='http://lorempixel.com/400/200/abstract/'> </img> "
+	end
+end
